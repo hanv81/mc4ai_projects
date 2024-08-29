90bb07774a6770b110960e77316a488d272732bc
Viet Ha Nguyen <hanv.cotai@gmail.com>
2024-08-29 14:49:12 +0700

    update

--- 7_movie_recsys/create_db.py
+++ 7_movie_recsys/create_db.py
@@ -13,38 +13,33 @@ data_path = 'data'
 
 def create_database():
     embs = []
-    df = pd.DataFrame(columns = ['Frame ID', 'Path'])
+    df = pd.DataFrame(columns = ['Frame ID', 'Name', 'Category'])
     
     for category in tqdm(os.listdir(data_path)):
         path = os.path.join(data_path, category)
         # print(category, os.listdir(path))
         for movie in tqdm(os.listdir(path)):
-            movie_path = os.path.join(path, movie)
-            for filename in os.listdir(movie_path):
-                if not filename.endswith('mp4'):
-                    continue
-                
-                file_path = os.path.join(movie_path, filename)
-                cap = cv2.VideoCapture(file_path)
-                frame_id = 0
-                frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-                print(f'\nProcessing {filename}/{frame_count}', )
-                while cap.isOpened():
-                    # if frame_id % 100 == 0:
-                    #     print(f'frame_id {frame_id}/{frame_count}')
-                    ret, frame = cap.read()
-                    if not ret:
-                        break
-                    # cv2.imshow('frame', frame)
+            movie_path = os.path.join(path, movie, 'video.mp4')
+            cap = cv2.VideoCapture(movie_path)
+            frame_id = 0
+            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            print(f'\nProcessing {movie}')
+            while cap.isOpened():
+                if frame_id % 100 == 0:
+                    print(f'frame_id {frame_id}/{frame_count}')
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                # cv2.imshow('frame', frame)
 
-                    image_data = processor_image(Image.fromarray(frame))
-                    _, embedding = model_image.encode(image_data, return_features=True)
-                    embs.append(embedding.flatten())
-                    frame_id += 1
-                    df.loc[len(df.index)] = [frame_id, file_path]
+                image_data = processor_image(Image.fromarray(frame))
+                _, embedding = model_image.encode(image_data, return_features=True)
+                embs.append(embedding.flatten())
+                df.loc[len(df.index)] = [frame_id, movie, category]
+                frame_id += 1
 
-                cap.release()
-                cv2.destroyAllWindows()
+            cap.release()
+            cv2.destroyAllWindows()
 
     embs = np.array(embs)
     index = faiss.IndexFlatL2(embs.shape[1])
